afecf33d2d0a8b09d0a5bedd0cda0916d5bbb113
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-01-14 09:45:33 +0200

    mokiniai

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-
+echo date("Y-m-d");
 $students = [
     "5a" => [
-        1 => [
-            //date(Y-m-d),
+        rand(1,30) => [
+            date("Y-m-d"),
             "agne",
             "kazlauskaite",
             "kontroliniai darbai" => [
@@ -12,8 +12,8 @@ $students = [
                 "anglu kalba" => 10
             ]
         ],
-        2 => [
-            //date(Y-m-d),
+        rand(1,30) => [
+            date("Y-m-d"),
             "petras",
             "petraitis",
             "kontroliniai darbai" => [
@@ -21,9 +21,88 @@ $students = [
                 "it" => 8,
                 "anglu kalba" => 9
             ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "jonas",
+            "jonaitis",
+            "kontroliniai darbai" => [
+                "matematika" => 8,
+                "it" => 5,
+                "anglu kalba" => 6
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "birute",
+            "dobkeviciute",
+            "kontroliniai darbai" => [
+                "matematika" => 9,
+                "it" => 9,
+                "anglu kalba" => 9
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "algirdas",
+            "jonusas",
+            "kontroliniai darbai" => [
+                "matematika" => 10,
+                "it" => 9,
+                "anglu kalba" => 6
+            ]
         ]
     ],
     "5b" => [
-
+        rand(1,30) => [
+            date("Y-m-d"),
+            "augustinas",
+            "dabkus",
+            "kontroliniai darbai" => [
+                "matematika" => 6,
+                "it" => 8,
+                "anglu kalba" => 7
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "lukas",
+            "macijauskas",
+            "kontroliniai darbai" => [
+                "matematika" => 6,
+                "it" => 7,
+                "anglu kalba" => 10
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "kajus",
+            "mazvydas",
+            "kontroliniai darbai" => [
+                "matematika" => 10,
+                "it" => 9,
+                "anglu kalba" => 5
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "antanas",
+            "antanaitis",
+            "kontroliniai darbai" => [
+                "matematika" => 8,
+                "it" => 7,
+                "anglu kalba" => 8
+            ]
+        ],
+        rand(1,30) => [
+            date("Y-m-d"),
+            "jonas",
+            "ambrazevicius",
+            "kontroliniai darbai" => [
+                "matematika" => 6,
+                "it" => 7,
+                "anglu kalba" => 10
+            ]
+        ]
     ]
 ];
