df363e953dac7174c7f234786d115e4197fadf18
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-01-14 09:31:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$students = [
+    "5a" => [
+        1 => [
+            //date(Y-m-d),
+            "agne",
+            "kazlauskaite",
+            "kontroliniai darbai" => [
+                "matematika" => 5,
+                "it" => 9,
+                "anglu kalba" => 10
+            ]
+        ],
+        2 => [
+            //date(Y-m-d),
+            "petras",
+            "petraitis",
+            "kontroliniai darbai" => [
+                "matematika" => 7,
+                "it" => 8,
+                "anglu kalba" => 9
+            ]
+        ]
+    ],
+    "5b" => [
+
+    ]
+];
